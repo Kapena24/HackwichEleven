b24423af3835fa90a4d4f5f6c42dc60cbf5ba2ad
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-11-21 13:14:38 -1000

    completed part 1 and 2

--- HackwichEleven/ViewController.swift
+++ HackwichEleven/ViewController.swift
@@ -8,28 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
-   
+    
+    
     @IBAction func myGuessButtonPressed(_ sender: Any) {
         let message = " The value is: \(currentValue) " + "\nThe Target value is: \(targetValue)"
         
-    let alert = UIAlertController(title: "Hello World", message: message , preferredStyle:.alert)
-                               
-               
+        let alert = UIAlertController(title: "Hello World", message: message , preferredStyle:.alert)
+        
+        
         let action = UIAlertAction(title: "Continue", style: .default, handler: nil)
-                               
-               
+        
+        
         alert.addAction(action)
-                               
-               
+        
+        
         present(alert, animated: true, completion: nil)
     }
-    
     @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 0
     var targetValue: Int = 0
     @IBAction func sliderHasMoved(_ sender: Any) {
-       
+        startNewRound()
         print("The Value of the slider is:\(slider.value)")
         currentValue = Int((slider.value))
     }
@@ -38,9 +37,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         currentValue = lroundf(slider.value)
         targetValue = Int.random(in:0...100)
-
+        startNewRound()
     }
-
-
+    
+    func startNewRound (){
+        targetValue = Int.random(in:0...100)
+        targetLabel.text = "\(targetValue)"
+       
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    
 }
-
